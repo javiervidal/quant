112b6f52e745c114b05c422adc5be9700e1d1010
Javier Vidal <zanaguara@gmail.com>
2012-06-18 12:51:51 +0200

    calculating simple moving average

--- lib/quant.rb
+++ lib/quant.rb
@@ -1,5 +1,20 @@
 require "quant/version"
 
 module Quant
-  # Your code goes here...
+
+  def self.sma(values, n)
+    acc = 0
+    values.inject_with_index([]) do |m, e, i|
+      acc += e
+      if i < (n - 1)
+        m << (0 / 0.0) # NaN
+      elsif i == (n - 1)
+        m << acc / n
+      else
+        acc = acc - values[i - n]
+        m << acc / n
+      end
+    end
+  end
+
 end
