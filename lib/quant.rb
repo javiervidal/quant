61c467f2aa919b1357cb75628a52d919605ba09d
Javier Vidal <zanaguara@gmail.com>
2012-06-18 17:40:29 +0200

    donchian channels

--- lib/quant.rb
+++ lib/quant.rb
@@ -7,7 +7,7 @@ module Quant
     values.inject_with_index([]) do |m, e, i|
       acc += e
       if i < (n - 1)
-        m << (0 / 0.0) # NaN
+        m << not_a_number
       elsif i == (n - 1)
         m << acc / n
       else
@@ -15,6 +15,25 @@ module Quant
         m << acc / n
       end
     end
+  end
+
+  def self.donchian_channel(ohlc, n)
+    dc = []
+    ohlc.each_with_index do |e, i|
+      if i < (n - 1)
+        dc << [not_a_number, not_a_number]
+      else
+        c = ohlc[(i - n + 1)..(i)].flatten
+        dc << [c.max, c.min]
+      end
+    end
+    dc
+  end
+
+  private
+
+  def self.not_a_number
+    0 / 0.0
   end
 
 end
