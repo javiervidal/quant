4f56ba106eb2cd3743a3e4190d9091fbdafa9e5e
Javier Vidal <zanaguara@gmail.com>
2012-06-19 11:40:11 +0200

    small typos fixed

--- test/test_atr.rb
+++ test/test_atr.rb
@@ -1,16 +1,6 @@
 require 'common_requires'
 
-class TestSma < MiniTest::Unit::TestCase
-
-  def test_eurusd_50_tr
-    c = Util.ohlc(Samples::Ohlc.eurusd_50_opens, Samples::Ohlc.eurusd_50_highs, Samples::Ohlc.eurusd_50_lows, Samples::Ohlc.eurusd_50_closes)
-    assert Util.compare_2_arrays_of_floats Quant.tr(c), Samples::Ttr.eurusd_50_ttr_tr
-  end
-
-  def test_usdjpy_150_tr
-    c = Util.ohlc(Samples::Ohlc.usdjpy_150_opens, Samples::Ohlc.usdjpy_150_highs, Samples::Ohlc.usdjpy_150_lows, Samples::Ohlc.usdjpy_150_closes)
-    assert Util.compare_2_arrays_of_floats Quant.tr(c), Samples::Ttr.usdjpy_150_ttr_tr
-  end
+class TestAtr < MiniTest::Unit::TestCase
 
   def test_eurusd_50_atr20
     c = Util.ohlc(Samples::Ohlc.eurusd_50_opens, Samples::Ohlc.eurusd_50_highs, Samples::Ohlc.eurusd_50_lows, Samples::Ohlc.eurusd_50_closes)
